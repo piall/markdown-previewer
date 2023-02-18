1251d63a12b44bfbd9301c56097c4f66c199a5dd
Md. Pial Ahamed <mdpialahamed@gmail.com>
2023-02-18 09:01:01 +0600

    Add layout for previewer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Editor } from './components';
 
 import './App.scss';
+import { Previewer } from './components/previewer';
 
 function App() {
   return (
     <div id="App">
       <div className="row gx-0">
-        <Editor className="col" />
+        <Editor className="col-md-6" />
+        <Previewer className="col-md-6" id="previewer-wrapper" />
       </div>
     </div>
   );
